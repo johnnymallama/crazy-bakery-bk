cf975508c8641e390c21bdf4ee8136b54edf9e53
rociasco26 <rociasco26@gmail.com>
2026-01-18 03:02:14 +0000

    ajuste ingredientes

--- src/main/java/uan/edu/co/crazy_bakery/application/mappers/IngredienteMapper.java
+++ src/main/java/uan/edu/co/crazy_bakery/application/mappers/IngredienteMapper.java
@@ -2,6 +2,7 @@ package uan.edu.co.crazy_bakery.application.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import uan.edu.co.crazy_bakery.application.dto.requests.CrearIngredienteDTO;
 import uan.edu.co.crazy_bakery.application.dto.responses.IngredienteDTO;
 import uan.edu.co.crazy_bakery.domain.model.Ingrediente;
@@ -9,6 +10,14 @@ import uan.edu.co.crazy_bakery.domain.model.Ingrediente;
 @Mapper(componentModel = "spring")
 public interface IngredienteMapper {
 
+    @Mappings({
+        @Mapping(target = "id", source = "ingrediente.id"),
+        @Mapping(target = "nombre", source = "ingrediente.nombre"),
+        @Mapping(target = "composicion", source = "ingrediente.composicion"),
+        @Mapping(target = "tipoIngrediente", source = "ingrediente.tipoIngrediente"),
+        @Mapping(target = "valor", source = "ingrediente.valor"),
+        @Mapping(target = "estado", source = "ingrediente.estado")
+    })
     IngredienteDTO ingredienteToIngredienteDTO(Ingrediente ingrediente);
 
     @Mapping(target = "id", ignore = true)
