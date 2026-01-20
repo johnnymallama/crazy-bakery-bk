e1601ae32297d39e2143537787d82dfde799682f
rociasco26 <rociasco26@gmail.com>
2026-01-20 00:36:14 +0000

    Delete Implement tipoIngrediente

--- src/main/java/uan/edu/co/crazy_bakery/application/services/TipoIngredienteService.java
+++
@@ -1,9 +0,0 @@
-package uan.edu.co.crazy_bakery.application.services;
-
-import uan.edu.co.crazy_bakery.domain.enums.TipoIngrediente;
-
-import java.util.List;
-
-public interface TipoIngredienteService {
-    List<TipoIngrediente> getAllTiposIngrediente();
-}
